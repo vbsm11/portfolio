e72e8c839f7281a2b72f57a56769f2f19b36226d
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-07-03 07:16:03 +0300

    template for main block

---
+++ src/main/Main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './Main.module.css'
+
+export function Main() {
+    return (
+        <div className={s.mainBlock}>
+            <div className={s.container}>
+                <div className={s.text}>
+                <span>
+                    Hi there
+                </span>
+                    <h1>
+                        I am Jeffrey Aaron
+                    </h1>
+                    <p>
+                        Frontend developer
+                    </p>
+                </div>
+                <div className={s.photo}>
+                </div>
+            </div>
+        </div>
+    );
+}
