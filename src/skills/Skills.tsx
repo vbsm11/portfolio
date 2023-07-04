75b60d583a97541b193bca5459e8d44c062b6c05
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-07-04 06:42:09 +0300

    template for skills block

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import s from './Skills.module.css'
+import containerS from '../common/styles/Container.module.css'
+import {Skill} from './skill/Skill';
 
 export function Skills() {
   return (
-    <div className={s.skills}>
-
+    <div className={s.skillsBlock}>
+        <div className={`${containerS.container} ${s.skillsContainer}`}>
+            <h2 className={s.title}>Skills</h2>
+            <div className={s.skills}>
+                <Skill iconLink='' title='HTML/CSS' description='"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'/>
+                <Skill iconLink='' title='JS' description='Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
+                <Skill iconLink='' title='React' description='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'/>
+            </div>
+        </div>
     </div>
   );
 }
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import s from './Skill.module.css'
 
-export function Skill() {
-  return (
-    <div className={s.skillBlock}>
+type SkillPropsType = {
+    iconLink: string
+    title: string
+    description: string
+}
 
-    </div>
-  );
+export function Skill(props: SkillPropsType) {
+    return (
+        <div className={s.skill}>
+            <div className={s.icon}>
+
+            </div>
+            <h3>{props.title}</h3>
+            <span className={s.description}>
+                {props.description}
+            </span>
+        </div>
+    );
 }
 
