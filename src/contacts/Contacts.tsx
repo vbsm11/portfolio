68ecbad1c526c4c75fef8c5b281f07ea0e5a7ab2
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-07-06 06:59:14 +0300

    template for contacts block

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Contacts.module.css'
 import containerS from '../common/styles/Container.module.css'
+import buttonS from '../common/styles/Button.module.css'
 
 export function Contacts() {
   return (
@@ -8,8 +9,11 @@ export function Contacts() {
         <div className={`${containerS.container} ${s.contactsContainer}`}>
             <h2 className={s.title}>Contacts</h2>
             <form className={s.form}>
-
+                <input type="text"/>
+                <input type="text"/>
+                <textarea/>
             </form>
+            <a className={buttonS.button} href="">Submit</a>
         </div>
     </div>
   );
